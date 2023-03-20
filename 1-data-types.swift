d8016de4e8e4ddea17fb51578159f0ae3ad1b08c
kahbyte <kahbyte@gmail.com>
2023-03-20 17:21:07 -0300

    :sparkles: finished days 0 and 1

---
+++ 1-data-types.swift
@@ -0,0 +1,30 @@
+//peidei pro stdin do c
+// Declare second integer, double, and String variables.
+
+var sumInt: Int = 0
+var sumDouble: Double = 0
+var finalString: String = ""
+
+// Read and save an integer, double, and String to your variables
+if let readInt = Int(readLine()!) {
+    sumInt = readInt + i
+}
+
+if let readDouble = Double(readLine()!) {
+    sumDouble = readDouble + d
+}
+
+if let readString = readLine() {
+    finalString =  s + readString
+}
+
+// Print the sum of both integer variables on a new line.
+print(sumInt)
+
+// Print the sum of the double variables on a new line.
+print(sumDouble)
+
+// Concatenate and print the String variables on a new line
+print(finalString)
+
+// The 's' variable above should be printed first.
